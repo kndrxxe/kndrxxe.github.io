1f78c99301ba97333aeae0665050d6bc5c837c6e
Kendrix Brosas <brosaskndrx05@gmail.com>
2024-12-28 01:52:07 +0800

    modified:   css/style.css modified:   index.html modified:   js/script.js modified:   manifest.json new file:   systems/1.png new file:   systems/2.png new file:   systems/3.png new file:   systems/4.png new file:   systems/5.png

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 window.onload = function () {
   Particles.init({
     selector: ".background",
-    maxParticles: 150,
-    connectParticles: true,
+    maxParticles: 500,
+    connectParticles: false,
     color: "#000000",
     responsive: [
       {
         breakpoint: 768,
         options: {
-          maxParticles: 100,
+          maxParticles: 500,
           color: "#000000",
-          connectParticles: true,
+          connectParticles: false,
         },
       },
       {
         breakpoint: 425,
         options: {
-          maxParticles: 80,
+          maxParticles: 500,
           color: "#000000",
-          connectParticles: true,
+          connectParticles: false,
         },
       },
       {
         breakpoint: 320,
         options: {
-          maxParticles: 60,
+          maxParticles: 500,
           color: "#000000",
-          connectParticles: true,
+          connectParticles: false,
         },
       },
     ],
